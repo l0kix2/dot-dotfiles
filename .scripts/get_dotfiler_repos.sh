b43bb34f05305fb57428ee9c3f1da938f1a7ff19
Kirill SIbirev <l0kix2@gmail.com>
2017-04-28 21:26:37 +0300

    Create get_dotfiler_repos.sh

---
+++ .scripts/get_dotfiler_repos.sh
@@ -0,0 +1,6 @@
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-common-sh.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-zsh.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-bash.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-oh-my-zsh.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-python.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-homebrew.git
