4773de99923aee31544b57750675618fd7f9d0aa
Kirill SIbirev <l0kix2@gmail.com>
2017-04-28 22:07:56 +0300

    Update install_things.sh

--- .scripts/install_things.sh
+++ .scripts/install_things.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# move to dot-homebrew (install-once)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 brew-install-packages.sh
 pyenv-install-pythons.sh
 pip-install-packages.sh
