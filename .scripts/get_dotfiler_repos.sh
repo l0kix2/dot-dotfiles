f3f5da2b372bf5bc4eb7c55611ca765bfb07fc1e
Kirill SIbirev <l0kix2@gmail.com>
2019-09-24 14:49:43 +0300

    Update get_dotfiler_repos.sh

--- .scripts/get_dotfiler_repos.sh
+++ .scripts/get_dotfiler_repos.sh
@@ -5,3 +5,4 @@ python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-bash.git
 python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-oh-my-zsh.git
 python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-python.git
 python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-homebrew.git
+python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-arcadia.git
