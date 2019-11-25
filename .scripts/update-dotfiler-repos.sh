abef7809f2f52612862add6f6d56c104455fd301
Kirill SIbirev <l0kix2@gmail.com>
2019-11-25 14:26:47 +0300

    Update update-dotfiler-repos.sh

--- .scripts/update-dotfiler-repos.sh
+++ .scripts/update-dotfiler-repos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find ~/.dotfiler -name '.git' | grep -v '.dotfiler/.git' | awk -F/ '{print $5}' | sort | diff ~/.conf/dot-repos.txt - | grep '>' | awk '{print $2}' | xargs -I{} rm -r  ~/.dotfiler/{}
-cat ~/.conf/dot-repos.txt | xargs -I {} python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-{}.git
+cat ~/.conf/dot-repos.txt | grep -v dotfiles | xargs -I {} python ~/.dotfiler/bin/dot add git@github.com:l0kix2/dot-{}.git
